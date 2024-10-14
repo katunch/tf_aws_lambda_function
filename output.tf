79792cae8f1231600103a65cd148e6995682e4e9
Silas Knobel <silas.knobel@katun.ch>
2024-10-14 14:22:52 +0200

    added invoke_arn

--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
 output "arn" {
+  description = "The ARN of the Lambda function"
   value = aws_lambda_function.fn.arn
+}
+output "invoke_arn" {
+  description = "ARN to be used for invoking Lambda Function from API Gateway"
+  value = aws_lambda_function.fn.invoke_arn
 }
